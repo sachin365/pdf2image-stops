0ea92013a57eab69587b538b09f0d8021a8e9e6d
Sachin Kafle <sachinkafle365@gmail.com>
2023-12-27 13:17:14 +0545

    main file for pdf2image

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pdf2image import convert_from_path
+from pdf2image.exceptions import (
+ PDFInfoNotInstalledError,
+ PDFPageCountError,
+ PDFSyntaxError
+)
+
+
+from pydantic import BaseModel
+
+
+class URL(BaseModel):
+    pdf_url: str
+    
+
+from pathlib import Path
+from glob import glob
+app=FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message":"Hello World from STOPS!!"}
+
+@app.post("/")
+async def pdftoimage(url: URL):
+    images = convert_from_path(url, first_page=1, last_page=1)
+    for i, image in enumerate(images):
+        fname = "image" + str(i) + ".png"
+        image.save(fname, "PNG")
+    return {"message": "success"}
